50e8d0b22a4639c166b04c85f4ef8d1326c7e3ae
Grubalub <69207566+Grubalub@users.noreply.github.com>
2022-10-07 16:34:21 +0100

    Add files via upload

---
+++ I am Poor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Gabriel  Cowan on 15/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
